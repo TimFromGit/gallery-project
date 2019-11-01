135bcd2d3b0114d4756b113088a12cee531fdaca
Tim <ti_mur@mail.ru>
2019-11-01 16:41:18 +0300

    create new component Item, make isLoading

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,38 @@
 import React from "react";
+import {Item} from "./Item";
 
 export class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            items: []
+            items: [],
+            isLoading: false
         };
     }
 
     componentDidMount() {
+        this.setState({
+            isLoading: true
+        })
         fetch('https://www.reddit.com/r/reactjs.json?limit=100')
-            .then(response => {
-                return response.json();
-            })
+            .then(response => response.json())
             .then(({data}) => {
                 this.setState({
-                    items: data.children
+                    items: data.children,
+                    isLoading: false
                 })
             });
     }
 
     render() {
-        const {items} = this.state;
-        return <div>
-            <h1>Top commented</h1>
-            {items.map(item => <div key={item.data.id}>
-                {item.data.thumbnail!=='self' ? <img src={item.data.thumbnail}/>: null}
-                <h3>{item.data.title}</h3>
-                <p>Number of comments: {item.data.num_comments}</p>
-                <a href={item.data.permalink} target="_blank">
-                    link
-                </a>
-            </div>)}
-
-        </div>;
+        const {items, isLoading} = this.state;
+        return (
+            <div>
+                <h1>Top commented</h1>
+                {isLoading
+                    ? <p>Loading...</p>
+                    : items.map(item => <Item key={item.data.id} data={item.data}/>
+                    )}
+            </div>)
     }
 }
---
+++ src/components/Item.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+export class Item extends Component {
+    render() {
+        const {data} = this.props;
+        return (
+            <div
+                style={{
+                    border: "1px solid black",
+                    marginBottom: "10px",
+                    padding: "5px"
+                }}>
+                {data.thumbnail !== 'self' ? <img src={data.thumbnail} alt=""/> : null}
+                <h3>{data.title}</h3>
+                <p>Number of comments: {data.num_comments}</p>
+                <a href={`https://www.reddit.com/${data.permalink}`} target="_blank"
+                   rel="noopener noreferrer">
+                    link
+                </a>
+            </div>
+        );
+    }
+};
