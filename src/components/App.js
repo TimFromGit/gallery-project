6d1b938c0aaf72fa8e57a4e261788ecb4dca71eb
Tim <ti_mur@mail.ru>
2019-11-01 17:40:51 +0300

    auto refresh button

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,19 @@ export class App extends React.Component {
         super();
         this.state = {
             items: [],
-            isLoading: false
+            isLoading: false,
+            enableAutoRefresh: false
         };
-    }
+    };
 
     componentDidMount() {
+        this.getItems();
+    };
+
+    getItems = () => {
         this.setState({
             isLoading: true
-        })
+        });
         fetch('https://www.reddit.com/r/reactjs.json?limit=100')
             .then(response => response.json())
             .then(({data}) => {
@@ -22,15 +27,33 @@ export class App extends React.Component {
                     isLoading: false
                 })
             });
-    }
+    };
+
+    updateAutoRefresh = () => {
+        this.setState(
+            state => ({
+                enableAutoRefresh: !state.enableAutoRefresh
+            }),
+            () => {
+                if (this.state.enableAutoRefresh) {
+                    this.autoRefresh = setInterval(this.getItems, 3000);
+                } else {
+                    clearInterval(this.autoRefresh);
+                }
+            }
+        );
+    };
 
     render() {
-        const {items, isLoading} = this.state;
+        const {items, isLoading, enableAutoRefresh} = this.state;
         const itemsSortByComments = items.sort(
-            (a,b) => b.data.num_comments - a.data.num_comments);
+            (a, b) => b.data.num_comments - a.data.num_comments);
         return (
             <div>
                 <h1>Top commented</h1>
+                <button type='button' style={{marginBottom: '15px'}} onClick={this.updateAutoRefresh}>
+                    {enableAutoRefresh ? "Stop" : "Start"} auto-refresh
+                </button>
                 {isLoading
                     ? <p>Loading...</p>
                     : itemsSortByComments.map(item => <Item key={item.data.id} data={item.data}/>
