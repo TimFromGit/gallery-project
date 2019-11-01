c8bc21b469cbc0e07039ff6b95719f3401a8cd20
Tim <ti_mur@mail.ru>
2019-11-01 18:58:23 +0300

    Item to functional component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,23 +1,20 @@
-import React, {PureComponent} from 'react';
+import React, {memo} from 'react';
 
-export class Item extends PureComponent {
-    render() {
-        const {data} = this.props;
-        return (
-            <div
-                style={{
-                    border: "1px solid black",
-                    marginBottom: "10px",
-                    padding: "5px"
-                }}>
-                {data.thumbnail !== 'self' ? <img src={data.thumbnail} alt=""/> : null}
-                <h3>{data.title}</h3>
-                <p>Number of comments: {data.num_comments}</p>
-                <a href={`https://www.reddit.com/${data.permalink}`} target="_blank"
-                   rel="noopener noreferrer">
-                    link
-                </a>
-            </div>
-        );
-    }
-};
+export const Item = memo(({data}) => {
+    return (
+        <div
+            style={{
+                border: "1px solid black",
+                marginBottom: "10px",
+                padding: "5px"
+            }}>
+            {data.thumbnail !== 'self' ? <img src={data.thumbnail} alt=""/> : null}
+            <h3>{data.title}</h3>
+            <p>Number of comments: {data.num_comments}</p>
+            <a href={`https://www.reddit.com/${data.permalink}`} target="_blank"
+               rel="noopener noreferrer">
+                link
+            </a>
+        </div>
+    );
+});
